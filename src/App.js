33e0f610614eb9c3ad8d92341ea1407ec41a647c
yoshio-the-end <shoma.yoshikawa9126@gmail.com>
2019-12-18 15:39:17 +0900

    initial commit
    Reactのルーティング練習

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/blog/:id" component={Blog} />
     </div>
-  );
+  </BrowserRouter>
+)
+
+const Home = () => {
+  return (
+    <div className="box">
+      <h1 className="title">ケミカルの香り</h1>
+      <p><Link to="/about">アバウトへ</Link></p>
+    </div>
+  )
 }
 
-export default App;
+const Blog = props => {
+  const { id } = props.match.params
+  return (
+    <div className="box">
+      <p className="title">{id}番目の記事です</p>
+    </div>
+  )
+}
+
+const About = () => {
+  return (
+    <div className="box">
+      <h1 className="title">さぁ、異臭を放ちくる</h1>
+      <p><Link to="/">ホームへ</Link></p>
+    </div>
+  )
+}
+
+export default App
